74db34b8af37d382b28b2963f9bc12b451f0afcb
Luis Horacio Botero Pena <lhbotero@celsia.com>
2024-09-05 22:24:54 -0500

    Ajustes Código

--- src/UserManagement.Infrastructure/obj/Debug/net8.0/UserManagement.Infrastructure.AssemblyInfo.cs
+++ src/UserManagement.Infrastructure/obj/Debug/net8.0/UserManagement.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+515aee623d77f6b8321bad35a2ca6d0c8af135a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
